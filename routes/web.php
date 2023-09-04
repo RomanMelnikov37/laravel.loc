9f6c651c7bee834d15f4b2b29b0494c015f7aa57
Roman <meln.r.1998@gmail.com>
2023-09-04 22:12:01 +0300

    testing controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', 'TestController@index');
+
+Route::resource('posts', 'PostController');
